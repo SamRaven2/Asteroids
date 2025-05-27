42729c39f52c6804cd195cdd6b71daa34b524434
Bats <samraven2@gmail.com>
2025-05-26 20:04:43 -0400

    Movement and Basic Asteroids

--- main.py
+++ main.py
@@ -1,21 +1,42 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    field = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         screen.fill((0,0,0))
-        player.draw(screen)
+
+        updatable.update(dt)
+        
+        for d in drawable:
+            d.draw(screen)
+        
         pygame.display.flip()
-        dt = clock.tick(60)
+        
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
